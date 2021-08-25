42db48be536bed2a1de324d8944e5115b988e538
admin <makoto.miyazaki@dataiku.com>
2021-08-25 10:45:40 +0200

    Saved scenario WESTNILEVIRUSMLOPS.Champion Challenger (id: CHAMPIONCHALLENGER)

--- scenarios/CHAMPIONCHALLENGER.py
+++ scenarios/CHAMPIONCHALLENGER.py
@@ -1,24 +1,27 @@
-# This sample code helps you get started with the custom scenario API.
-#For more details and samples, please see our Documentation
+import dataiku 
 from dataiku.scenario import Scenario
 
-# The Scenario object is the main handle from which you initiate steps
+model_id = "mFwVBlwL" # change this to your model id
 scenario = Scenario()
 
-# A few example steps follow
+def get_active_model_auc():
+    saved_model = dataiku.Model(model_id)
+    for model in saved_model.list_versions():
+        if model['active']:
+            model_score = model['snippet']['auc']
+            version_id = model['versionId'] 
+    return version_id, model_score
 
-# Building a dataset
-scenario.build_dataset("customers_prepared", partitions="2015-01-03")
+# get active model id and version 
+past_version_id, past_model_score = get_active_model_auc()
 
-# Controlling the train of a dataset
-train_ret = scenario.train_model("uSEkldfsm")
-trained_model = train_ret.get_trained_model()
-performance = trained_model.get_new_version_metrics().get_performance_values()
-if performance["AUC"] > 0.85:
-    trained_model.activate_new_version()
+# retrain model (this will activate it by default)
+scenario.train_model(model_id)
 
-# Sending custom reports
-sender = scenario.get_message_sender("mail-scenario", "local-mail") # A messaging channel
-sender.set_params(sender="dss@company.com", recipient="data-scientists@company.com")
+# get newly activated model id and version
+new_version_id, new_model_score = get_active_model_auc()
 
-sender.send(subject="The scenario is doing well", message="All is good")
+if new_model_score < past_model_score:
+    # if new model performance worse, reactivate previous version
+    dataiku.Model(model_id).activate_version(past_version_id)
+
